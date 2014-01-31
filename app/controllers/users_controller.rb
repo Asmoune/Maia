9cd9e66d1dd840d6c35d2226423414b2a36ff581
asma <asma.alibert@gmail.com>
2014-01-31 18:26:58 +0100

    Layout of the "New Vote" pop-in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
       flash[:success] = "Profile updated"
       redirect_to @user
     else
-      render 'edit'
+      render 'show'
     end
   end
 
